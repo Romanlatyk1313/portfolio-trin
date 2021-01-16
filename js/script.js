90981605865cb55d96eb62c4cf85f00c861d6115
Romanlatyk1313 <74365757+Romanlatyk1313@users.noreply.github.com>
2021-01-17 02:10:29 +0300

    new

---
+++ js/script.js
@@ -0,0 +1,35 @@
+//let position = 0;
+//const slidesToShow = 1;
+//const slidesToScroll = 1;
+//const container = document.querySelector('.slider-container');
+//const track = document.querySelector('.slider-track');
+//const btnPrev = document.querySelector('.btn-prev');
+//const btnNext = document.querySelector('.btn-next');
+//const itemsCount =items.length;
+//const itemWidth = container.clientWidth / slidesToShow;
+//const movePosition = slidesToScroll * itemWidth;
+//items.forEach((item) => {
+//   item.style.minWidth = `${itemWidth}px`;
+//});
+//btnNext.addEventListener('click', () => {
+//   const itemsLeft = itemsCount - (Math.ads(position) + slidesToShow * itemWidth) / itemWidth;
+//   position -= itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+//   setPosition();
+//   checkBtns();
+//});
+//btnPrev.click(function () {
+//   const itemsLeft = Math.abs(position) / itemWidth;
+//   position += itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+//   setPosition();
+//   checkBtns();
+//});
+//const setPosition = () => {
+//   track.style.transform = `translateX(${position}px)`
+
+//};
+//const checkBtns = () => {
+//   btnPrev.disabled = position === 0;
+//   btnNext.disabled = position <= -(itemsCount - slidesToShow) * itemWidth;
+
+//};
+//checkBtns();
